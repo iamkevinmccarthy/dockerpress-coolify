3b88c0da39262683837d51afbd91d017ca7d3e4f
Kevin C. McCarthy <iamkevinmccarthy@users.noreply.github.com>
2025-07-26 14:44:17 -0400

    Update entrypoint-custom.sh

--- entrypoint-custom.sh
+++ entrypoint-custom.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
-if [ -n "${VIRTUAL_HOST}" ] && [ ! -f "/usr/local/lsws/conf/certs/${VIRTUAL_HOST}/fullchain.pem" ]; then
-  /usr/local/lsws/admin/misc/acme.sh --issue -d ${VIRTUAL_HOST} -d www.${VIRTUAL_HOST} -w /var/www/html --force --email ${ADMIN_EMAIL}
+if [ ! -f "/root/.acme.sh/acme.sh" ]; then
+  curl https://get.acme.sh | sh
+  /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 fi
-/usr/local/lsws/admin/misc/acme.sh --install-cronjob
+if [ -n "${VIRTUAL_HOST}" ] && [ ! -f "/usr/local/lsws/conf/certs/${VIRTUAL_HOST}/fullchain.cer" ]; then
+  /root/.acme.sh/acme.sh --issue -d ${VIRTUAL_HOST} -d www.${VIRTUAL_HOST} -w /var/www/html --force --email ${ADMIN_EMAIL}
+fi
+/root/.acme.sh/acme.sh --install-cronjob
 exec /entrypoint.sh "$@"
